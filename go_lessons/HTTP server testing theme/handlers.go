1e15b4f4e09faab31df6b24eab0356441f54c2b9
Egor <egorko25@gmail.com>
2022-11-18 18:14:50 +0300

    edited UserViewHandler, added header Content-Type: application/json

---
+++ go_lessons/HTTP server testing theme/handlers.go
@@ -0,0 +1,47 @@
+package HTTP_server_testing_theme
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// StatusHandler send 200:ok on any request
+func StatusHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte(`{"status":"ok"}`))
+}
+
+// User — основной объект для теста.
+type User struct {
+	ID        string
+	FirstName string
+	LastName  string
+}
+
+// UserViewHandler — хендлер, который нужно протестировать.
+func UserViewHandler(users map[string]User) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		userId := r.URL.Query().Get("user_id")
+		if userId == "" {
+			http.Error(rw, "userId is empty", http.StatusBadRequest)
+			return
+		}
+
+		user, ok := users[userId]
+		if !ok {
+			http.Error(rw, "user not found", http.StatusNotFound)
+			return
+		}
+
+		jsonUser, err := json.Marshal(user)
+		if err != nil {
+			http.Error(rw, "can't provide a json. internal error", http.StatusInternalServerError)
+			return
+		}
+
+		rw.Header().Add("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		rw.Write(jsonUser)
+	}
+}
